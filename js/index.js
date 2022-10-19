5901156e0f8587a0f038d9b72e7695236743e7da
katalba <katherine.albanicg@gmail.com>
2022-10-19 11:03:13 -0300

    error

--- js/index.js
+++ js/index.js
@@ -1,42 +1,67 @@
-alert("Ingrese el nro del producto que desea comprar, para salir ingrese 0");
-let productos = parseInt(prompt("1- Jean Azul $14000 2- Jean Rojo $14500 3- Jean Blanco $14000 4- Jean Amarillo $17000"));
-let cantidad;
-let total = 0;
+const products = [
+    {name: "Jean Azul", price: 14000},
+    {name: "Jean Rojo", price: 14500},
+    {name: "Jean Blanco", price: 14000},
+    {name: "Jean Amarillo", price: 17000}
+];
 
-while(productos != 0) {
-    switch (productos) {
-        case 1:
-            cantidad = parseInt(prompt("El producto seleccionado es Jean Azul, indique cuántos desea comprar"))
-            total += seleCantidad(cantidad, 14000)
-        break;
+let shopping_car = []
 
-        case 2:
-            cantidad = parseInt(prompt("El producto seleccionado es Jean Rojo, indique cuántos desea comprar"))
-            total += seleCantidad(cantidad, 14500)
-        break;
+let selection = prompt("Bienvenidos a Valkiria: ¿Desea ver nuestros productos? Escriba Sí o No");
 
-        case 3:
-            cantidad = parseInt(prompt("El producto seleccionado es Jean Blanco, indique cuántos desea comprar"))
-            total += seleCantidad(cantidad, 14000)
-        break;
-
-        case 4:
-            cantidad = parseInt(prompt("El producto seleccionado es Jean Amarillo, indique cuántos desea comprar"))
-            total += seleCantidad(cantidad, 17000)
-        break;
-
-        default:
-        break;
-    }
-    let agregar = prompt("¿Desea agregar más productos? Escriba si o no.");
-    if (agregar == "si"){
-        productos = parseInt(prompt("1- Jean Azul $14000 2- Jean Rojo $14500 3- Jean Blanco $14000 4- Jean Amarillo $17000"));
-    }
-     else{
-        no = prompt("El total de su compra es: ", total)
-    }
+while (selection != "si" && selection != "no") {
+alert ("Por favor, escriba Sí o No")
+selection = prompt("Bienvenidos a Valkiria: ¿Desea ver nuestros productos? Escriba Sí o No");
 }
 
-function seleCantidad(cantidad, precio){
-    return cantidad * precio
+if (selection == "si") {
+    let acu_products = products.map (
+        (product) => product.name + "  " + "$" + product.price
+        );
+    alert (acu_products.join (" - "))
+} else if (selection == "no") {
+    alert ("Una lástima que abandones el sitio, hay cosas muy lindas para vos. Hasta pronto :(")
 }
+
+while (selection != "no") {
+    let product = prompt("Escriba el nombre del producto que desea comprar")
+    let price = 0
+
+    if (product == "jean azul" || product == "jean rojo" || product == "jean blanco" || product == "jean amarillo") {
+        switch(product){
+            case "jean azul":
+                price: 14000;
+                break
+            case "jean rojo":
+                price: 14500;
+                break
+            case "jean blanco":
+                price: 14000;
+                break
+            case "jean amarillo":
+                price: 17000;
+                break
+                default:
+                break;
+        }
+        let quentify = parseInt(prompt("Indique la cantidad que sea comprar"))
+
+        shopping_car.push({product, quentify, price})
+        console.log(shopping_car)
+    } else {
+        alert ("Lo siento, el producto que elegiste no está disponible en nuestra tienda")
+    }
+
+    selection = prompt("¿Desea agregar más productos al carrito? Escriba Sí o No")
+
+    while (selection == "no"){
+        alert ("Detalle de tu compra: ", shopping_car)
+        shopping_car.forEach((final_car) => {
+            alert(`producto: ${final_car.product}, cantidad: ${final_car.quentify}, total a pagar: ${final_car.quentify * final_car.price} `)
+        })
+        break;
+    }
+    
+   
+}
+
