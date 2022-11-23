6ab74fc8070d6e6687226c73f441fd16214f22b2
katalba <katherine.albanicg@gmail.com>
2022-11-23 12:23:44 -0300

    problem

--- js/cart.js
+++ js/cart.js
@@ -1,37 +1,39 @@
-const btn_cart = document.getElementById("show_cart");
+// OPEN & CLOSE (CART)
+ const btn_cart = document.getElementById("show_cart");
 
-btn_cart.addEventListener("click", function () {
+ btn_cart.addEventListener("click", function () {
 
-    let cart = document.getElementById("cart");
+     let cart = document.getElementById("cart");
 
-    if (cart.style.display == "none") {
-        cart.style.display = "block";
-    }
-    else {
-        cart.style.display = "none";
-    }
-})
+     if (cart.style.display == "none") {
+         cart.style.display = "block";
+     }
+     else {
+         cart.style.display = "none";
+     }
+ })
 
+// CART
+
+// SE MANTIENEN LOS PRODUCTOS EN EL CART
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
+// CREAMOS BOTON PARA AGREGAR PRODUCTOS
 let btn_compra = document.querySelectorAll(".agregar-carrito");
-
-console.log(btn_compra);
-
 
 for(let boton of btn_compra){
 
     boton.addEventListener("click" , carga_productos )
 }
 
-
+// SE ORGANIZAN LAS CAJAS PARA EXTRAER LA INFO AL CART
 function carga_productos(e){
 
     let hijo = e.target;
     let padre = hijo.parentNode;
     let abuelo = padre.parentNode;
 
-
+// SE SELECCIONA EL ELEMENTO DE CADA CAJA DESEADO
     let nombre_producto = padre.querySelector("h5").textContent;
     let precio_producto = padre.querySelector(".price").textContent;
     let img_producto = abuelo.querySelector("img").src;
@@ -44,12 +46,14 @@ function carga_productos(e){
         cantidad: 1
     }
 
+// PUSHEAMOS EL PRODUCTO AL CART
     cart.push(product);
     save_local();
 
     mostrar_carrito(product);
 }
 
+// SE ALMACENAN LOS DATOS EN LOCAL STORAGE
 const save_local = () => {
     localStorage.setItem("cart", JSON.stringify(cart));
 };
@@ -62,9 +66,8 @@ function mostrar_carrito( product ){
                       <td>${product.nombre}</td>
                       <td>${product.cantidad}</td>
                       <td>${product.precio}</td>
-                      <td><button class="btn btn-danger borrar_elemento">Borrar</td>`;
-    
-    console.log( fila );      
+                      <td><button class="btn btn-outline-danger borrar_elemento"><i class="fa-solid fa-trash-can"></i></td>`;
+         
     let tabla = document.getElementById("t-body");
     tabla.append( fila );
 
